fbeff21e89a16798d39ed628de9f660021b18462
Takafumi Sekiguchi <t.sekiguchi@sproutatwork.com>
2018-01-10 13:11:07 -0500

    Add phinx

---
+++ db/migrations/20180108213640_my_first_migration.php
@@ -0,0 +1,29 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class MyFirstMigration extends AbstractMigration
+{
+    public function up()
+    {
+        $this->execute("CREATE DATABASE IF NOT EXISTS tutorial DEFAULT CHARACTER SET 'utf8' COLLATE 'utf8_general_ci'");
+
+        $this->execute("CREATE TABLE IF NOT EXISTS news (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            title varchar(128) NOT NULL,
+            slug varchar(128) NOT NULL,
+            text text NOT NULL,
+            PRIMARY KEY (id),
+            KEY slug (slug)
+        )");
+        $this->execute("INSERT INTO news (
+            title,
+            slug,
+            text
+        ) VALUES
+        ('hello', 'hello', 'world'),
+        ('こんにちは', 'konnichiwa', '世界')
+        ");
+    }
+}
