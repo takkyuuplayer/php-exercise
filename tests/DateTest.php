f57860f13d2c4f3b6c62e8ff129bf2b85297b52b
Takafumi <t.sekiguchi@sproutatwork.com>
2018-06-20 13:20:46 -0400

    Fix test

--- tests/DateTest.php
+++ tests/DateTest.php
@@ -6,15 +6,6 @@ class DateTest extends TestCase
 {
     public function testDATE_ISO8601()
     {
-        $this->assertSame(
-            '2018-05-29T17:00:00-0700',
-            date(DATE_ISO8601),
-            ''
-        );
-    }
-
-    public function test_date()
-    {
-        echo date('Y-m-dTH:i:sZ');
+        $this->assertSame('1970-01-01T00:00:00+0000', date(DATE_ISO8601, 0));
     }
 }
