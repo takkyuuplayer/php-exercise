0596da238504bdd0499851e547ca3dae236e796e
Takafumi <t.sekiguchi@sproutatwork.com>
2018-07-15 22:58:02 -0400

    Add some test

---
+++ tests/ClosureTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ClosureTest extends TestCase
+{
+    public function testThisObjectInClosure()
+    {
+        $closure = function() {
+            return get_class($this);
+        };
+
+        $this->assertSame(__CLASS__, $closure());
+    }
+}
+
---
+++ tests/GuzzleHttp/PoolTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\GuzzleHttp;
+
+use GuzzleHttp\Pool;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
+class PoolTest extends \PHPUnit\Framework\TestCase
+{
+    public function testPool()
+    {
+        $client = new Client();
+        $requests = function ($total) {
+            for ($i = 0; $i < $total; $i++) {
+                yield new Request('GET', 'https://foo.google.co.jp');
+            }
+        };
+
+        $pool = new Pool(
+            $client,
+            $requests(10),
+            [
+                'concurrency' => 2,
+                'fulfilled' => function ($response, $index) {
+                    $this->assertSame(200, $response->getStatusCode());
+                },
+                'rejected' => function ($exception, $index) {
+                    var_dump($exception->getResponse());
+                }
+            ]
+        );
+
+        $promise = $pool->promise();
+        $promise->wait();
+    }
+}
