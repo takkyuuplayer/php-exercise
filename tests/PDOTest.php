72107ec3145ab6a14193b414ea55cb5134902b87
Takafumi SEKIGUCHI <takkyuuplayer@gmail.com>
2020-04-27 23:51:22 +0000

    Apply fixes from StyleCI

--- tests/PDOTest.php
+++ tests/PDOTest.php
@@ -40,9 +40,10 @@ SQL
 
         $this->assertEquals(
             [
-            ['user_id' => 1, 'balance' => 10000],
-            ['user_id' => 2, 'balance' => 100000],
-            ], $stmt->fetchAll(\PDO::FETCH_ASSOC)
+                ['user_id' => 1, 'balance' => 10000],
+                ['user_id' => 2, 'balance' => 100000],
+            ],
+            $stmt->fetchAll(\PDO::FETCH_ASSOC)
         );
 
         // UPDATE
@@ -57,9 +58,10 @@ SQL
 
         $this->assertEquals(
             [
-            ['user_id' => 1, 'balance' => 5000],
-            ['user_id' => 2, 'balance' => 105000],
-            ], $stmt->fetchAll(\PDO::FETCH_ASSOC)
+                ['user_id' => 1, 'balance' => 5000],
+                ['user_id' => 2, 'balance' => 105000],
+            ],
+            $stmt->fetchAll(\PDO::FETCH_ASSOC)
         );
 
         // Delete
@@ -73,8 +75,9 @@ SQL
 
         $this->assertEquals(
             [
-            ['user_id' => 2, 'balance' => 105000],
-            ], $stmt->fetchAll(\PDO::FETCH_ASSOC)
+                ['user_id' => 2, 'balance' => 105000],
+            ],
+            $stmt->fetchAll(\PDO::FETCH_ASSOC)
         );
     }
 }
